4cc10657ce0af5f6a1de5748d56edd5f68d34770
DRinsvind <adskiyprikol@gmail.com>
2018-08-25 14:40:33 +0300

    create express app, connect to db in mlab.com, user and recipe mongoose schema

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config({ path: './variables.env' })
+
+const Recipe = require('./models/Recipe')
+const User = require('./models/User')
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(()=>console.log('DB connected'))
+  .catch(err => console.error(err))
+
+
+// Initializes application  
+const app = express()
+
+const PORT = process.env.PORT || 4444
+
+app.listen(PORT, ()=>{
+  console.log(`Server listening on port ${PORT}`)
+})
