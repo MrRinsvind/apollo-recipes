e598169518151b6e4c63f7f4323d27bf524206f7
Ilya Lucenko <adskiyprikol@gmail.com>
2018-09-17 17:01:36 +0300

    Preparation for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: './variables.env' })
@@ -69,6 +70,13 @@ app.use('/graphql',
     }
   }))
 )
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (res,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 4444
 
