90f33a57b5832eccbf67d3151d81347a256d1e4c
DRinsvind <adskiyprikol@gmail.com>
2019-01-14 10:23:27 +0200

    add file validation fix

--- client/src/app/common/widgets/Error.jsx
+++ client/src/app/common/widgets/Error.jsx
@@ -1,1 +1,7 @@
-export default Error = ({ error }) => <p>{error.message}</p>
+export default Error = ({ error }) => {
+  let errorText = error.message
+  if(errorText === "GraphQL error: Cannot read property 'pipe' of undefined"){
+    errorText = "Image is required for api"
+  }
+  return  <p>{errorText}</p>
+}
